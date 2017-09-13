0aa780c858593732fdc0b4f841d78b20b15bd558
Walison Moreira <walison.moreira@gmail.com>
2017-09-13 16:55:40 -0300

    Melhoria na escrita do teste.

--- src/main/java/br/com/caelum/leilao/teste/TesteDoAvaliador.java
+++ src/main/java/br/com/caelum/leilao/teste/TesteDoAvaliador.java
@@ -7,20 +7,26 @@ import br.com.caelum.leilao.servico.Avaliador;
 
 public class TesteDoAvaliador {
   public static void main(String[] args) {
+    // Parte 1: Montar cenário.
     Usuario joao = new Usuario("João");
     Usuario jose = new Usuario("José");
     Usuario maria = new Usuario("Maria");
 
     Leilao leilao = new Leilao("Playstation 3 Novo");
 
-    leilao.propoe(new Lance(joao, 250)); //Menor lance dado.
-    leilao.propoe(new Lance(jose, 300)); 
-    leilao.propoe(new Lance(maria, 400)); //Maior lance dado.
+    leilao.propoe(new Lance(joao, 250)); // Menor lance dado.
+    leilao.propoe(new Lance(jose, 300));
+    leilao.propoe(new Lance(maria, 400)); // Maior lance dado.
 
+    //Parte 2: Executar ação.
     Avaliador leiloeiro = new Avaliador();
     leiloeiro.avalia(leilao);
 
-    System.out.println(leiloeiro.getMaiorLance());
-    System.out.println(leiloeiro.getMenorLance());
+    // Parte 3: Validar do resultado.
+    double maiorEsperado = 400;
+    double menorEsperado = 250;
+
+    System.out.println(maiorEsperado == leiloeiro.getMaiorLance());
+    System.out.println(menorEsperado == leiloeiro.getMenorLance());
   }
 }
