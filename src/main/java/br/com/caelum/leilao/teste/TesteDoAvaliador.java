65995241889fd7ce503c7bf4d4235ec808a355a3
Walison Moreira <walison.moreira@gmail.com>
2017-09-13 17:30:08 -0300

    Evolução do teste para usar JUnit.

--- src/main/java/br/com/caelum/leilao/teste/TesteDoAvaliador.java
+++ src/main/java/br/com/caelum/leilao/teste/TesteDoAvaliador.java
@@ -1,12 +1,17 @@
 package br.com.caelum.leilao.teste;
 
+import org.junit.Test;
+
 import br.com.caelum.leilao.dominio.Lance;
 import br.com.caelum.leilao.dominio.Leilao;
 import br.com.caelum.leilao.dominio.Usuario;
 import br.com.caelum.leilao.servico.Avaliador;
+import junit.framework.Assert;
 
 public class TesteDoAvaliador {
-  public static void main(String[] args) {
+
+  @Test
+  public void main() { // Deve ser "public void", não pode ser estático nem receber argumentos.
     // Parte 1: Montar cenário.
     Usuario joao = new Usuario("João");
     Usuario jose = new Usuario("José");
@@ -26,7 +31,7 @@ public class TesteDoAvaliador {
     double maiorEsperado = 400;
     double menorEsperado = 250;
 
-    System.out.println(maiorEsperado == leiloeiro.getMaiorLance());
-    System.out.println(menorEsperado == leiloeiro.getMenorLance());
+    Assert.assertEquals(maiorEsperado, leiloeiro.getMaiorLance(), 0.00001);
+    Assert.assertEquals(menorEsperado, leiloeiro.getMenorLance(), 0.00001);
   }
 }
